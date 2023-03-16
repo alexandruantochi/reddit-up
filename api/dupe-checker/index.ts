cecb21fc1009590525b0c5f0139ba7e5112a557b
Alexandru Antochi <alexandru.antochi@gmail.com>
2023-03-16 15:27:27 +0000

    added dupe checker function (#19)
    Co-authored-by: Alexandru Antochi <alexandru.antochi@outlook.com>

---
+++ api/dupe-checker/index.ts
@@ -0,0 +1,42 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+
+    if (!req.headers['content-length'] || parseInt(req.headers['Content-length']) > 10000 || !context.req.body) {
+        context.res = {
+            status: 403
+        }
+        return;
+    }
+
+    let dupeEtags = new Set();
+    let dupeUrls: string[] = [];
+    let requests: Promise<void>[] = [];
+    let urlsToCheck: string[] = context.req.body;
+
+    for (let url of urlsToCheck) {
+        requests.push(fetch(url, { method: 'HEAD' }).then(
+            data => {
+                let etag = data.headers.get("etag");
+                if (dupeEtags.has(etag)) {
+                    dupeUrls.push(url);
+                } else {
+                    dupeEtags.add(etag);
+                }
+            },
+            err => {
+                console.error(err);
+            }
+        ));
+    }
+
+    await Promise.all(requests);
+
+    context.res = {
+        status: 200,
+        body: dupeUrls
+    }
+};
+
+
+export default httpTrigger;
