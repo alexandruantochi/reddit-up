61785ffed2476330fcc33c7f1ca84b34e53bff12
Alexandru Antochi <alexandru.antochi@gmail.com>
2023-03-16 15:25:51 +0000

    Revert "Remove duplicate content (#16)"
    This reverts commit 39483d6ac2a11b3ff0467506467a227d19de6e50.

--- ui/src/app/components/user-page/user-page.component.ts
+++ ui/src/app/components/user-page/user-page.component.ts
@@ -1,8 +1,8 @@
-import {Component, ViewChild} from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ContentRetrieverService } from 'src/app/services/content-retriever.service';
-import { UserAboutData, UserSubmittedData } from 'src/app/constants/types';
-import {GalleryComponent, GalleryItem} from 'ng-gallery';
+import { UserAboutData, UserPost, UserSubmittedData } from 'src/app/constants/types';
+import { GalleryItem, IframeItem, ImageItem, VideoItem } from 'ng-gallery';
 import { Title } from '@angular/platform-browser';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpErrorResponse } from "@angular/common/http";
@@ -15,13 +15,10 @@ import { HttpErrorResponse } from "@angular/common/http";
 })
 
 export class UserPageComponent {
-  @ViewChild(GalleryComponent) gallery?: GalleryComponent;
 
   public userSubmittedData: UserSubmittedData | undefined;
   public retrievingData: boolean;
   public galleryList: GalleryItem[] = [];
-
-  public loadedContentSet: Set<string> = new Set<string>();
 
   constructor(
     private route: ActivatedRoute,
@@ -100,34 +97,25 @@ export class UserPageComponent {
         continue;
       }
 
-      this.contentRetriever.loadImages(entry.data.thumbnail).subscribe(
-        (thumbData) => {
-          if(!this.loadedContentSet.has(thumbData)) {
-          if (entry.data.domain === 'i.imgur.com' && entry.data.url.includes('.gifv')) {
-            let newUrl = entry.data.url.split('/')[3].split('.')[0];
-            this.gallery?.addVideo({
-              src: [{
-                url: `https://i.imgur.com/${newUrl}.mp4`,
-                type: 'video/mp4'
-              }] as any,
-              thumb: thumbData,
-              poster: thumbData,
-              autoplay: true,
-              controls: true,
-              loop: true
-            })
+      if (entry.data.domain === 'i.imgur.com' && entry.data.url.includes('.gifv')) {
+        let newUrl = entry.data.url.split('/')[3].split('.')[0];
+        images.push(new VideoItem({
+           src: [ {
+            url : `https://i.imgur.com/${newUrl}.mp4`,
+            type: 'video/mp4' } ] as any,
+          thumb: entry.data.thumbnail,
+          poster: entry.data.thumbnail,
+          autoplay: true,
+          controls: true,
+          loop: true
+        }))
 
-          } else if (entryDataType === 'image') {
-            this.gallery?.addImage({src: entry.data.url, thumb: thumbData});
-          } else if (entryDataType === 'rich:video' && entry.data.url.includes('redgifs')) {
-            this.gallery?.addIframe({src: entry.data.url.replace('watch', 'ifr'), thumb: thumbData});
-          }
-
-          this.loadedContentSet.add(thumbData);
-        }
-          }
-      )
-
+      }
+      else if (entryDataType === 'image') {
+        images.push(new ImageItem({ src: entry.data.url, thumb: entry.data.thumbnail }));
+      } else if (entryDataType === 'rich:video' && entry.data.url.includes('redgifs')) {
+        images.push(new IframeItem({ src: entry.data.url.replace('watch', 'ifr'), thumb: entry.data.thumbnail }));
+      }
 
     }
     this.galleryList = images;
