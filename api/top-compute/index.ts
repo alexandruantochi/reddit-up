4a7921e8320c0a7705797512fc9d0cbd6867c7fe
Alexandru Antochi <alexandru.antochi@gmail.com>
2023-03-30 21:16:23 +0300

    top compute now looks at users visited in the past 10 days (#65)

--- api/top-compute/index.ts
+++ api/top-compute/index.ts
@@ -6,10 +6,11 @@ import { BlobServiceClient, BlockBlobClient } from "@azure/storage-blob"
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
 
     const numberOfUsers = parseInt(process.env.TOP_USER_COUNT) || 10;
+    const visitedInTheLastDays = parseInt(process.env.VISITED_LAST_DAYS) || 10;
     const topN = getBlobClient(numberOfUsers);
 
     try {
-        const topUserData = JSON.stringify(await getTopUsers(numberOfUsers));
+        const topUserData = JSON.stringify(await getTopUsers(numberOfUsers, visitedInTheLastDays));
         const uploadBlobResponse = await topN.upload(topUserData, topUserData.length);
     } catch (err) {
         console.error('Failed to get or update top user data.');
@@ -18,15 +19,19 @@ const timerTrigger: AzureFunction = async function (context: Context, myTimer: a
     console.log(`Succesfully updated top${numberOfUsers} file.`);
 };
 
-const getTopUsers = async function (numberOfUsers: number) {
+const getTopUsers = async function (numberOfUsers: number, pastDays: number) {
     let container = getUsersContainer();
     const topN = await container.items
         .query(
             {
-                query: "SELECT TOP @numberOfUsers c.username, c.visited FROM c ORDER BY c.visited DESC",
-                parameters: [{ name: "@numberOfUsers", value: numberOfUsers }]
+                query: "SELECT TOP @numberOfUsers c.username, c.visited FROM c WHERE GetCurrentTimestamp()/1000-c._ts < @pastDays ORDER BY c.visited DESC",
+                parameters: [
+                    { name: "@numberOfUsers", value: numberOfUsers },
+                    { name: "@pastDays", value: pastDays * 86400 }
+                ]
             })
         .fetchNext();
+
     return topN.resources;
 }
 
